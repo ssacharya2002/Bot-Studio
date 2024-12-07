6f17bed33416d19ac1656c8e5faac78769004c44
Shaktishankar Acharya <86561217+ssacharya2002@users.noreply.github.com>
2024-12-07 23:49:58 +0530

    chatbot added in welcome page

--- app/(dashboard)/welcome/page.tsx
+++ app/(dashboard)/welcome/page.tsx
@@ -1,13 +1,29 @@
-import Navbar from "./components/Navbar";
-import Home from "./landingpage";
+"use client";
 
-function page() {
+import { usePathname } from "next/navigation";
+import Navbar from "./components/Navbar";
+import Home from "./landingpage"; 
+import Script from "next/script";
+
+function Page() {
+  const pathname = usePathname();
+
+  const isTargetPage = pathname === "/welcome";
+
   return (
     <div>
       <Navbar />
       <Home />
+      {/* //@ts-expect-error appid */}
+      {isTargetPage && (
+        <Script
+          src="https://res.cloudinary.com/dt43piwsk/raw/upload/v1733587999/ygx2bnrvmrsgevsb7cvf.js"
+          data-appid={process.env.NEXT_PUBLIC_CHATBOT_APP_ID}
+          strategy="lazyOnload"
+        />
+      )}
     </div>
   );
 }
 
-export default page;
+export default Page;
