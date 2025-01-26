b0efe8e3f474b1b0d3c9db59403628f86121ca53
Shaktishankar Acharya <86561217+ssacharya2002@users.noreply.github.com>
2025-01-26 13:43:18 +0530

    made client component

--- app/(dashboard)/project/[botId]/(routes)/integration/page.tsx
+++ app/(dashboard)/project/[botId]/(routes)/integration/page.tsx
@@ -1,9 +1,14 @@
-import { ApiAlert } from "@/components/ui/api-alert";
+"use client";
 
-const Page = ({ params }: { params: { botId: string } }) => {
-  const appId = params.botId;
-  const scriptTag = `<script appid="${appId}" src="${process.env.NEXT_PUBLIC_CHATBOT_INTEGRATION_SCRIPT}"></script>`;
-  
+import { ApiAlert } from "@/components/ui/api-alert";
+import { useParams } from "next/navigation";
+import React from "react";
+
+const Page = () => {
+  const { botId } = useParams();
+
+  const scriptTag = `<script appid="${botId}" src="${process.env.NEXT_PUBLIC_CHATBOT_INTEGRATION_SCRIPT}"></script>`;
+
   return (
     <div className="p-4 md:p-10">
       <ApiAlert title="CHATBOT_INTEGRATION_SCRIPT" description={scriptTag} />
